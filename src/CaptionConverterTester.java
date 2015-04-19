b0f1592e0f1b924a904279c3bd68f6138eba9f5a
Nick Ginther <nginth@gmail.com>
2015-04-19 18:36:46 -0500

    added some testing

--- src/CaptionConverterTester.java
+++ src/CaptionConverterTester.java
@@ -1,9 +1,9 @@
 
 public class CaptionConverterTester {
 	public static void main(String args[]){
-		CaptionConverter cc = new CaptionConverter();
-		String filename = "captions.srt";
-		
-		cc.convertCaptions(filename);
+//		CaptionConverter cc = new CaptionConverter();
+//		String filename = "captions.srt";
+//		
+//		cc.convertCaptions(filename);
 	}
 }

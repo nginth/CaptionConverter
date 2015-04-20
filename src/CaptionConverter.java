4a50afa2535780da1e5ddc01a1099b20d900778d
Nick Ginther <nginth@gmail.com>
2015-04-19 19:21:48 -0500

    added cli argument support

--- src/CaptionConverter.java
+++ src/CaptionConverter.java
@@ -9,32 +9,44 @@ import java.util.Scanner;
 
 
 public class CaptionConverter {
+	private boolean printTimes;
+	private String fileName;
+	private String targetFilename;
 	
-//	public static void main(String[] args){
-//		CaptionConverter cc = new CaptionConverter();
-//		String filename = "captionTest.srt";
-//		StringBuffer sb = new StringBuffer();
-//		try{
-//			Scanner sc = new Scanner(new File(filename));
-//			while(sc.hasNextLine()){
-//				sb.append(sc.nextLine());
-//				sb.append("\n");
-//			}
-//			String block = sb.toString();
-//			System.out.println(block);
-//			CaptionBlock cb = cc.createCaptionBlock(block);
-//			System.out.println(cb.getCaption());
-//		}catch(Exception e){
-//			e.printStackTrace();
-//		}
-//		
-//	}
-
-	public void getCaptionsAsText(String filename){
+	public static void main(String[] args){
+		CaptionConverter cc;
+		if(args[0].equals("-help"))
+			printHelp();
+		if(!checkFileExtension(args[0])) //check to see if args[0] is a .srt
+			throw new IllegalArgumentException("File must be a .srt in the correct format. See http://en.wikipedia.org/wiki/SubRip for format examples.");
+		else if(args.length == 3){ //has a third argument that may or may not be -t
+			if(!args[2].equals("-t"))
+				throw new IllegalArgumentException("-help for all arguments");
+			else{
+				cc = new CaptionConverter(args[0], args[1], true);
+			}
+		}
+		else if(args.length == 2){
+			cc = new CaptionConverter(args[0], args[1], false);
+		}
+		else
+			throw new IllegalArgumentException("Too few or too many arguments.");
+		
+		cc.getCaptionsAsText();
+	}
+	
+	public CaptionConverter(String fileName, String targetFilename, boolean printTimes){
+		this.printTimes = printTimes;
+		this.fileName = fileName;
+		this.targetFilename = targetFilename;
+	}
+	
+	private void getCaptionsAsText(){
 		try {
-            File file = new File("captionsText.txt");
+            File file = new File(targetFilename);
             BufferedWriter output = new BufferedWriter(new FileWriter(file));
-            for(CaptionBlock cb : convertCaptionsIntoBlocks(filename))
+            ArrayList<CaptionBlock> captionBlocks = convertCaptionsIntoBlocks(fileName);
+            for(CaptionBlock cb : captionBlocks)
             	output.write(cb.getCaption());
             output.close();
         } catch ( IOException e ) {
@@ -55,15 +67,18 @@ public class CaptionConverter {
 				StringBuffer sb = new StringBuffer();
 				
 				//when line.length == 0, create new CaptionBlock
-				while(line.length() != 0){
-					sb.append(fileScanner.nextLine());
+				while(line.length() != 0 && fileScanner.hasNextLine()){
+					sb.append(line);
 					sb.append("\n");
+					line = fileScanner.nextLine();
 				}
-				captionBlocks.add(createCaptionBlock(sb.toString()));
-				fileScanner.nextLine(); //throw away the empty line
+				CaptionBlock cb = createCaptionBlock(sb.toString());
+				System.out.println(cb);
+				captionBlocks.add(cb);
 			}
 		}catch(Exception e){
 			System.out.println("Error while processing file: " + e.getMessage());
+			e.printStackTrace();
 		}
 		
 		return captionBlocks;
@@ -86,11 +101,31 @@ public class CaptionConverter {
 		}
 		text = sb.toString();
 		
-		if(text.charAt(0) == '>'){ //specific to my .srt files, each sentence starts with ">> ", this removes it
+		if(text.length() > 0 && text.charAt(0) == '>'){ //specific to my .srt files, each sentence starts with ">> ", this removes it
 			text = text.substring(3);
 		}
 		
 		return new CaptionBlock(text, number, time);
+		
+	}
+	
+	private static boolean checkFileExtension(String file){
+		//is file an .srt?
+		return file.substring(file.lastIndexOf("."), file.length()).equals(".srt");
+	}
+	
+	private static void printHelp(){
+		System.out.println("CaptionConverter converts .srt subtitle files to a more readable format.");
+		System.out.println();
+		System.out.println("How to use:");
+		System.out.println("CaptionConverter.java [<fileName>.srt] [<targetFileName>.txt]");
+		System.out.println("Where <fileName>.srt is the file containing the captions "
+				+ "and <targetFileName>.txt is the file to which you would like the output to be written to.");
+		System.out.println("If <targetFileName>.txt does not exist it will be created. If it does exist it will be overwritten!");
+		System.out.println();
+		System.out.println("Arguments: \n------------");
+		System.out.println("-help      Shows this help page.");
+		System.out.println("-t         Outputs timestamps and line numbers");
 		
 	}
 	
@@ -116,8 +151,17 @@ public class CaptionConverter {
 		public int getLineNumber(){
 			return lineNumber;
 		}
-	
 		
+		public String toString(){
+			StringBuffer sb = new StringBuffer();
+			sb.append(lineNumber);
+			sb.append(" ");
+			sb.append(time);
+			sb.append(" ");
+			sb.append(captionText);
+			return sb.toString();
+		}
+			
 	}
 	
 }
--- src/CaptionConverterTester.java
+++
@@ -1,9 +0,0 @@
-
-public class CaptionConverterTester {
-	public static void main(String args[]){
-//		CaptionConverter cc = new CaptionConverter();
-//		String filename = "captions.srt";
-//		
-//		cc.convertCaptions(filename);
-	}
-}
